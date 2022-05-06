38655eee4953618bb49b0b8672ebf26af3414888
Santiago Vallejo <santiagovallejoc@gmail.com>
2022-05-06 00:42:53 -0500

    bash script

---
+++ docker.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+sudo apt update
+sudo apt upgrade -y
+sudo apt install apt-transport-https ca-certificates curl software-properties-common -y
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu focal stable"
+apt-cache policy docker-ce
+sudo apt install docker-ce -y
+sudo apt  install docker-compose -y
+sudo usermod -aG docker ${USER}
+su - ${USER}
+groups
+sudo usermod -aG docker ubuntu
