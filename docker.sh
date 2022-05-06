57dbaae0b686549b8619dede6aae0224b4abc124
Santiago Vallejo <santiagovallejoc@gmail.com>
2022-05-06 00:45:22 -0500

    Update docker.sh

--- docker.sh
+++ docker.sh
@@ -1,13 +1,25 @@
 #!/bin/bash
+
+# Update the apt package index
 sudo apt update
+
+# Upgrade all packages
 sudo apt upgrade -y
+
+# Install Docker
 sudo apt install apt-transport-https ca-certificates curl software-properties-common -y
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu focal stable"
 apt-cache policy docker-ce
 sudo apt install docker-ce -y
+
+# Install docker-compose
 sudo apt  install docker-compose -y
+
+# Executing the Docker Command Without Sudo
 sudo usermod -aG docker ${USER}
 su - ${USER}
 groups
+
+# Change the user ubuntu to the user you want to run docker
 sudo usermod -aG docker ubuntu
